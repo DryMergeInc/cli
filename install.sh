7673fdbd55c23b2afef2c5da3643693b21737cc3
samuelbrashears <samuel.brashears@gmail.com>
2023-08-31 12:54:21 -0700

    test

--- install.sh
+++ install.sh
@@ -3,17 +3,42 @@
 # Define variables
 REPO="DryMergeInc/cli"
 LATEST_RELEASE=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep "tag_name" | awk -F '"' '{print $4}')
-DOWNLOAD_URL="https://github.com/$REPO/releases/download/$LATEST_RELEASE/drycli"  # Replace 'drycli' with your actual artifact name
+DOWNLOAD_FILE="drycli.zip"
+EXECUTABLE_NAME="drycli_exec"
+
+# Detect the operating system
+OS=$(uname)
+
+if [ "$OS" == "Darwin" ]; then
+  # macOS
+  DOWNLOAD_URL="https://github.com/$REPO/releases/download/$LATEST_RELEASE/graphapi_null_x86_64-apple-darwin.zip"
+  ARTIFACT_NAME="graphapi_null_x86_64-apple-darwin.zip"
+  echo "Detected macOS. Downloading latest release: $LATEST_RELEASE..."
+elif [ "$OS" == "Linux" ]; then
+  # Linux
+  DOWNLOAD_URL="https://github.com/$REPO/releases/download/$LATEST_RELEASE/graphapi_null_x86_64-unknown-linux-musl.tar.gz"
+  ARTIFACT_NAME="graphapi_null_x86_64-unknown-linux-musl.tar.gz"
+  echo "Detected Linux. Downloading latest release: $LATEST_RELEASE..."
+else
+  echo "Unsupported OS. Exiting."
+  exit 1
+fi
 
 # Download latest release
-echo "Downloading latest release: $LATEST_RELEASE..."
-curl -L -o drycli $DOWNLOAD_URL
+curl -L -o $DOWNLOAD_FILE $DOWNLOAD_URL
+
+# Unzip the downloaded file
+echo "Unzipping downloaded file..."
+if [ "$OS" == "Darwin" ]; then
+  unzip $DOWNLOAD_FILE -d $EXECUTABLE_NAME
+elif [ "$OS" == "Linux" ]; then
+  tar -xzf $DOWNLOAD_FILE -C $EXECUTABLE_NAME
+fi
 
 # Make it executable
-chmod +x drycli
+chmod +x $EXECUTABLE_NAME/drycli
 
 # Move it to /usr/local/bin or any other directory in $PATH
-sudo mv drycli /usr/local/bin/
+sudo mv $EXECUTABLE_NAME/drycli /usr/local/bin/dry
 
-echo "drycli installed successfully."
-
+echo "$EXECUTABLE_NAME installed successfully."
